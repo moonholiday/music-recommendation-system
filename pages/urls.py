fc38dee3d19165d0e093b4ef4bbe4d6cccfc1645
moonholiday <727moonholiday@gmail.com>
2022-02-03 18:25:40 +0545

    spotify api auth

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, FaqPageView, GenrePageView
+from .views import *
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
     path('faq/', FaqPageView.as_view(), name='faq'),
-    path('genre/', GenrePageView.as_view(), name='genre'),
+    path('genre/', GenrePageView.as_view() , name='genre'),
     path('', HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-
-# Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
@@ -14,3 +14,8 @@ class FaqPageView(TemplateView):
 
 class GenrePageView(TemplateView):
     template_name = 'genre.html'
+    sp = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id="b4dad3bdf5144e6f8f408ec2f6f278a3", client_secret="a1e9ff23036a4444abcf6067fd63c2ca"))
+
+    result = sp.search(q="bipul chettri", limit=40)
+    for idx, track in enumerate(result['tracks']['items']):
+        print(idx, track['name'])
