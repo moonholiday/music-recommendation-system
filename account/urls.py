ef3eca5034fcd519a8bdd7e2a24f52fb66696c0b
moonholiday <727moonholiday@gmail.com>
2022-02-04 20:15:17 +0545

    password chagne added

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
 
     path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
