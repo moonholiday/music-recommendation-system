fbbfee42ae4229c430d27e29caca17238e894c5e
moonholiday <727moonholiday@gmail.com>
2022-02-05 21:47:19 +0545

    user management

--- account/forms.py
+++ account/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
-class UserRegistration(forms.ModelForm):
-    password = forms.CharField(label='Password',widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password',widget=forms.PasswordInput)
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
@@ -14,8 +15,16 @@ class UserRegistration(forms.ModelForm):
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
 
-
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
 
     path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
@@ -14,5 +18,10 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/dnoe/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
