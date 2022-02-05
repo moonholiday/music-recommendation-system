b3f0e9f9ec86323683561f1e481f021232d0b970
moonholiday <727moonholiday@gmail.com>
2022-02-05 12:46:31 +0545

    password reset

--- account/urls.py
+++ account/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # reset password urls
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/dnoe/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
