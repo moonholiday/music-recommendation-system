6525c99bd0f7b2c45f41af4dba66173eb8605574
moonholiday <727moonholiday@gmail.com>
2022-04-30 19:44:05 +0545

    final

--- pages/urls.py
+++ pages/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
     path('genre/', views.GenrePageView, name='genre'),
-    path('genre_playlist/', views.genre_playlist, name='genre_playlist'),
     path('discover/', views.discover, name='discover'),
     path('free_music/', views.free_music, name='free_music'),
     path('add_music/', views.add_music, name='add_music'),
--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from .models import *
 from .spotify_client import *
 import json
@@ -15,7 +15,10 @@ from .forms import AudioForm
 from django.contrib.auth.decorators import login_required
 from operator import is_not
 from functools import partial
-from selenium import webdriver
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+
+
 
 client_id="b4dad3bdf5144e6f8f408ec2f6f278a3"
 client_secret="a1e9ff23036a4444abcf6067fd63c2ca"
@@ -30,90 +33,86 @@ class FaqPageView(TemplateView):
     template_name = 'faq.html'
 
 def GenrePageView(request):
+    x = request.POST.get('x')
+    print(x)
     sp = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
     genres = sp.categories(country='US', locale=None, limit=10, offset=0)
     # print(genres)
     # print(genre1)
-    genre_list = genres['categories']['items']
-    x = []
-    # print(genre_list)
-    # if request.GET.get('x') is not None:
+    if x is None:
+        genre_list = genres['categories']['items']
+        x = []
+        # print(genre_list)
+        # if request.GET.get('x') is not None:
+        context = {'genre_list': genre_list}
+        return render(request, 'genre.html', context = context)
 
-
-
-    context = {'genre_list': genre_list}
-    return render(request, 'genre.html', context = context)
+    if x is not None:
+        playlist = sp.category_playlists(category_id=x)
+        pl_list = playlist['playlists']['items']
+        context = {'pl_list': pl_list}
+        print(pl_list)
+        return render(request, 'genre.html', context=context)
     # test = sp.audio_features('68Dni7IE4VyPkTOH9mRWHr')
     # test1 = test
     # print(test1)
-class LocalStorage:
+# class LocalStorage:
+#
+#     def __init__(self, driver) :
+#         self.driver = driver
+#
+#     def __len__(self):
+#         return self.driver.execute_script("return window.localStorage.length;")
+#
+#     def items(self) :
+#         return self.driver.execute_script( \
+#             "var ls = window.localStorage, items = {}; " \
+#             "for (var i = 0, k; i < ls.length; ++i) " \
+#             "  items[k = ls.key(i)] = ls.getItem(k); " \
+#             "return items; ")
+#
+#     def keys(self) :
+#         return self.driver.execute_script( \
+#             "var ls = window.localStorage, keys = []; " \
+#             "for (var i = 0; i < ls.length; ++i) " \
+#             "  keys[i] = ls.key(i); " \
+#             "return keys; ")
+#
+#     def get(self, key):
+#         return self.driver.execute_script("return window.localStorage.getItem(arguments[0]);", key)
+#
+#     def set(self, key, value):
+#         self.driver.execute_script("window.localStorage.setItem(arguments[0], arguments[1]);", key, value)
+#
+#     def has(self, key):
+#         return key in self.keys()
+#
+#     def remove(self, key):
+#         self.driver.execute_script("window.localStorage.removeItem(arguments[0]);", key)
+#
+#     def clear(self):
+#         self.driver.execute_script("window.localStorage.clear();")
+#
+#     def __getitem__(self, key) :
+#         value = self.get(key)
+#         if value is None :
+#           raise KeyError(key)
+#         return value
+#
+#     def __setitem__(self, key, value):
+#         self.set(key, value)
+#
+#     def __contains__(self, key):
+#         return key in self.keys()
+#
+#     def __iter__(self):
+#         return self.items().__iter__()
+#
+#     def __repr__(self):
+#         return self.items().__str__()
 
-    def __init__(self, driver) :
-        self.driver = driver
 
-    def __len__(self):
-        return self.driver.execute_script("return window.localStorage.length;")
 
-    def items(self) :
-        return self.driver.execute_script( \
-            "var ls = window.localStorage, items = {}; " \
-            "for (var i = 0, k; i < ls.length; ++i) " \
-            "  items[k = ls.key(i)] = ls.getItem(k); " \
-            "return items; ")
-
-    def keys(self) :
-        return self.driver.execute_script( \
-            "var ls = window.localStorage, keys = []; " \
-            "for (var i = 0; i < ls.length; ++i) " \
-            "  keys[i] = ls.key(i); " \
-            "return keys; ")
-
-    def get(self, key):
-        return self.driver.execute_script("return window.localStorage.getItem(arguments[0]);", key)
-
-    def set(self, key, value):
-        self.driver.execute_script("window.localStorage.setItem(arguments[0], arguments[1]);", key, value)
-
-    def has(self, key):
-        return key in self.keys()
-
-    def remove(self, key):
-        self.driver.execute_script("window.localStorage.removeItem(arguments[0]);", key)
-
-    def clear(self):
-        self.driver.execute_script("window.localStorage.clear();")
-
-    def __getitem__(self, key) :
-        value = self.get(key)
-        if value is None :
-          raise KeyError(key)
-        return value
-
-    def __setitem__(self, key, value):
-        self.set(key, value)
-
-    def __contains__(self, key):
-        return key in self.keys()
-
-    def __iter__(self):
-        return self.items().__iter__()
-
-    def __repr__(self):
-        return self.items().__str__()
-
-def genre_playlist(request):
-    storage = LocalStorage(driver)
-
-    print(storage.get("genre"))
-    # r = slugify(x)
-    # f = re.sub('\-', '', r)
-    # print(f)
-    sp = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
-    playlist = sp.category_playlists(category_id='rock')
-    pl_list = playlist['playlists']['items']
-    context = {'pl_list': pl_list}
-    # print(playlist)
-    return render(request, 'genre_playlist.html', context=context)
 
 
 def discover(request):
